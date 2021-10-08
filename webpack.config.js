8de32c5c235785ad48772fac66f243719942f1f3
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-10-08 15:41:13 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'awesome.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+}
